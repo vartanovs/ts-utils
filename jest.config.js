e7f026a490fe9dad41b31fb3721664b322032d79
Serge Vartanov <vartanov.s@gmail.com>
2020-08-23 18:19:55 -0700

    update testing configuration

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   clearMocks: true,                                 // Clear mock calls between every test
   collectCoverageFrom: ['packages/**/*.{js,ts}'],   // Generate coverage report from packages dir
   coverageDirectory: 'coverage',                    // Specify coverage report output dir
-  roots: [ '<rootDir>/packages' ],                  // All package files are in the src directory
+  roots: ['<rootDir>/packages'],                    // All test files are in the packages directory
   testRegex: '.+\.test\.ts$',                       // Look for file.test.ts files
   transform: {
     '^.+\\.ts$': 'ts-jest',                         // Specify use of ts-jest for all .ts files
