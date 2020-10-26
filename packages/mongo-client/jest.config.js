cbd30ba0da39de02b03911ec9ef14617995dffe3
Serge Vartanov <vartanov.s@gmail.com>
2020-10-25 18:04:17 -0700

    mongo client configuration

---
+++ packages/mongo-client/jest.config.js
@@ -0,0 +1,7 @@
+const config = require('../../jest.config');
+
+module.exports = {
+  ...config,
+  collectCoverageFrom: ['src/**/*.{js,ts}'],  // Generate coverage report from src dir
+  roots: ['<rootDir>/tests'],                 // All test files are in the tests directory
+};
